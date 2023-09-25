42ca3f0c64040ca8ab42c90cb1c41c2657127890
fulgorum <eduardo.caballerpons@cieep.com>
2023-09-25 20:08:13 +0200

    Arreglo de bug que no aparecian en el manifes.xml motosActivity ni BicisActivity. Solucionado.

--- Ejercicio2Clase/app/src/main/java/eduardo/caballer/ejercicio2clase/BicisActivity.java
+++ Ejercicio2Clase/app/src/main/java/eduardo/caballer/ejercicio2clase/BicisActivity.java
@@ -3,12 +3,32 @@ package eduardo.caballer.ejercicio2clase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BicisActivity extends AppCompatActivity {
+    private TextView txtLabel;
+    private EditText txtMarca;
+    private EditText txtPulgadas;
+    private Button btnCancelar;
+    private Button btnCrear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bicis);
+
+        inicializarVista();
+    }
+
+    private void inicializarVista() {
+        txtLabel = findViewById(R.id.txtLabelBici);
+
+        txtMarca = findViewById(R.id.txtMarcaBici);
+        txtPulgadas = findViewById(R.id.txtPulgadaBici);
+
+        btnCancelar = findViewById(R.id.btnCancelarBici);
+        btnCrear = findViewById(R.id.btnCrearBici);
     }
 }
